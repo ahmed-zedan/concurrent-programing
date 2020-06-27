dbbe5bbd04a1362305f04f0fce4f93ef1df56a97
Ahmed Zedan <a.z.flefel@gmail.com>
2020-06-27 17:25:55 +0200

    upgrade intent service to job intent service

--- app/src/main/java/com/example/zedan/concurrentprogramming/MyIntentService.kt
+++ app/src/main/java/com/example/zedan/concurrentprogramming/MyIntentService.kt
@@ -4,23 +4,17 @@ import android.app.IntentService
 import android.content.Intent
 import android.content.Context
 import android.util.Log
-
-// IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-private const val ACTION_FOO = "com.example.zedan.concurrentprogramming.action.FOO"
-private const val ACTION_BAZ = "com.example.zedan.concurrentprogramming.action.BAZ"
-
-private const val EXTRA_PARAM1 = "com.example.zedan.concurrentprogramming.extra.PARAM1"
-private const val EXTRA_PARAM2 = "com.example.zedan.concurrentprogramming.extra.PARAM2"
+import androidx.core.app.JobIntentService
 
 /**
- * An [IntentService] subclass for handling asynchronous task requests in
+ * An [JobIntentService] subclass for handling asynchronous task requests in
  * a service on a separate handler thread.
  * helper methods.
  */
-class MyIntentService : IntentService("MyIntentService") {
+class MyIntentService : JobIntentService() {
 
-    override fun onHandleIntent(intent: Intent?) {
-        when (intent?.action) {
+    override fun onHandleWork(intent: Intent) {
+        when (intent.action) {
             ACTION_FOO -> {
                 val param1 = intent.getStringExtra(EXTRA_PARAM1)
                 val param2 = intent.getStringExtra(EXTRA_PARAM2)
@@ -53,6 +47,15 @@ class MyIntentService : IntentService("MyIntentService") {
     companion object {
 
         private const val TAG = "RunnerCodeIntentService"
+
+        // IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
+        private const val ACTION_FOO = "com.example.zedan.concurrentprogramming.action.FOO"
+        private const val ACTION_BAZ = "com.example.zedan.concurrentprogramming.action.BAZ"
+
+        private const val EXTRA_PARAM1 = "com.example.zedan.concurrentprogramming.extra.PARAM1"
+        private const val EXTRA_PARAM2 = "com.example.zedan.concurrentprogramming.extra.PARAM2"
+
+        private const val JOB_ID = 1001
         /**
          * Starts this service to perform action Foo with the given parameters. If
          * the service is already performing a task this action will be queued.
@@ -66,7 +69,7 @@ class MyIntentService : IntentService("MyIntentService") {
                 putExtra(EXTRA_PARAM1, param1)
                 putExtra(EXTRA_PARAM2, param2)
             }
-            context.startService(intent)
+            enqueueWork(context, MyIntentService::class.java, JOB_ID, intent)
         }
 
         /**
@@ -82,7 +85,7 @@ class MyIntentService : IntentService("MyIntentService") {
                 putExtra(EXTRA_PARAM1, param1)
                 putExtra(EXTRA_PARAM2, param2)
             }
-            context.startService(intent)
+            enqueueWork(context, MyIntentService::class.java, JOB_ID, intent)
         }
     }
 }
