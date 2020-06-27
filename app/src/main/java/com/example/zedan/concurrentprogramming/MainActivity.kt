3dfef790442801c0012358a3a6b3a49603b9c461
Ahmed Zedan <a.z.flefel@gmail.com>
2020-06-27 04:29:43 +0200

    Update MainActivity.kt
    Delay execution of a Runnable object

--- app/src/main/java/com/example/zedan/concurrentprogramming/MainActivity.kt
+++ app/src/main/java/com/example/zedan/concurrentprogramming/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.zedan.concurrentprogramming
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.os.SystemClock
 import android.util.Log
 import android.widget.ScrollView
 import com.example.zedan.concurrentprogramming.databinding.ActivityMainBinding
@@ -29,7 +30,14 @@ class MainActivity : AppCompatActivity() {
      * Run some code
      */
     private fun runCode(){
-        Handler().post { log("Operation from runnable 1.") }
+
+        Handler().postAtTime( {log("Posting at a certain time.")} , SystemClock.uptimeMillis() + 4000)
+
+        //The value that you pass into the postDelayed function
+        //indicates how long you want to wait from the time the code was executed.
+        Handler().postDelayed({ log("Operation from runnable 1.") }, 3000)
+        Handler().postDelayed({ log("Operation from runnable 2.") }, 2000)
+        Handler().postDelayed({ log("Operation from runnable 3.") }, 1000)
         log("Synchronous operation 1.")
         log("Synchronous operation 2.")
         log("Synchronous operation 3.")
