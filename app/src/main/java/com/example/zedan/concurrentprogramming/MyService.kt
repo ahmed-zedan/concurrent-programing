9e8c2248ae5aaa997c132618d953b66b05d89ebf
Ahmed Zedan <a.z.flefel@gmail.com>
2020-06-27 19:32:45 +0200

    run a service in the foreground

--- app/src/main/java/com/example/zedan/concurrentprogramming/MyService.kt
+++ app/src/main/java/com/example/zedan/concurrentprogramming/MyService.kt
@@ -1,11 +1,15 @@
 package com.example.zedan.concurrentprogramming
 
-import android.app.Service
+import android.app.*
+import android.content.Context
 import android.content.Intent
 import android.media.MediaPlayer
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
 import android.util.Log
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 
 class MyService : Service() {
 
@@ -19,6 +23,46 @@ class MyService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         return START_STICKY
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(): String{
+        val channelId = "my_service"
+        val channelName = "Music Service"
+        NotificationChannel(channelId, channelName, NotificationManager.IMPORTANCE_DEFAULT).also {
+            it.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as
+                    NotificationManager
+
+            notificationManager.createNotificationChannel(it)
+        }
+
+        return channelId
+    }
+
+
+
+    private fun displayForegroundNotification(){
+        val channelId =
+            if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+                createNotificationChannel()
+            }else{
+                ""
+            }
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent =
+            PendingIntent.getActivity(this, 0, notificationIntent, 0)
+
+        val notification = NotificationCompat.Builder(this, channelId)
+            .setContentTitle("Playing music")
+            .setContentText(AUDIO_FILE)
+            .setSmallIcon(R.drawable.ic_run)
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setWhen(0)
+            .build()
+
+        startForeground(1001, notification)
     }
 
     fun startMusic(){
@@ -36,6 +80,8 @@ class MyService : Service() {
             it.start()
         }
 
+        displayForegroundNotification()
+
     }
 
     fun stopMusic(){
@@ -43,6 +89,8 @@ class MyService : Service() {
             player.stop()
         }catch (e: UninitializedPropertyAccessException){
         }
+
+        stopForeground(false)
 
     }
 
