491370c0af77bcf4ae81a5ca20fe09d2cdfb8708
Ahmed Zedan <a.z.flefel@gmail.com>
2020-06-27 04:07:12 +0200

    getting started video

--- app/src/main/java/com/example/zedan/concurrentprogramming/MainActivity.kt
+++ app/src/main/java/com/example/zedan/concurrentprogramming/MainActivity.kt
@@ -2,10 +2,61 @@ package com.example.zedan.concurrentprogramming
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import android.widget.ScrollView
+import com.example.zedan.concurrentprogramming.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        // Initialize view binding for view object references
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding){
+            runButton.setOnClickListener { runCode() }
+            clearButton.setOnClickListener { clearOutput() }
+        }
+
+    }
+
+    /**
+     * Run some code
+     */
+    private fun runCode(){
+        log("Running code.")
+    }
+
+    /**
+     * clear log display
+     */
+    private fun clearOutput(){
+        binding.logDisplay.text = ""
+    }
+
+    /**
+     * Log output to logcat and the screen
+     */
+    @Suppress("SameParameterValue")
+    private fun log(message: String) {
+        Log.i(LOG_TAG, message)
+        binding.logDisplay.append(message + "\n")
+        scrollTextToEnd()
+    }
+
+    /**
+     * Scroll to the end. Wrapped in post() function so it's the last thing to happen
+     */
+    private fun scrollTextToEnd(){
+        Handler().post { binding.scrollView.fullScroll(ScrollView.FOCUS_DOWN) }
+    }
+
+    companion object{
+        private const val LOG_TAG = "MainActivity"
     }
 }
